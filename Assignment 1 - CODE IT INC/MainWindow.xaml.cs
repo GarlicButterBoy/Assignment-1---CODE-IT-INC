6982b246ca5e332b3cf0e1472340454259ae1f56
Nick Sturch-Flint <nick.rulez@live.com>
2020-10-03 08:26:21 -0400

    Add project files.

---
+++ Assignment 1 - CODE IT INC/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Assignment_1___CODE_IT_INC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BillingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //Global Scope Object
+        Project tempProject = new Project();
+        //I should probably create an array of project classes to keep track of the various data, then use for loops when clicking on the listbox?
+        //something like that
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateProject_Click(object sender, RoutedEventArgs e)
+        {
+            Project project = new Project();
+            project.ProjectName = txtProjectName.Text;
+            project.ProjectBudget = double.Parse(txtBudget.Text);
+            project.ProjectAmountSpent = double.Parse(txtSpent.Text);
+            project.ProjectHoursRemaining = int.Parse(txtHoursLeft.Text);
+            project.ProjectStatus = cbStatusList.Text;
+            lbProjectList.Items.Add(project.ProjectName);
+            tempProject = project;
+        }
+
+        private void listBoxClick(object sender, EventArgs e)
+        {
+            ProjectInfo newWindow = new ProjectInfo();
+            newWindow.Show();
+
+            newWindow.txtProjectNameDisplay.Text = tempProject.ProjectName;
+            newWindow.txtBudgetDisplay.Text = Convert.ToString(tempProject.ProjectBudget);
+            newWindow.txtTimeRemainingDisplay.Text = Convert.ToString(tempProject.ProjectHoursRemaining);
+            newWindow.txtSpentDisplay.Text = Convert.ToString(tempProject.ProjectAmountSpent);
+            newWindow.cbStatusDisplay.Text = tempProject.ProjectStatus;
+        }
+    }
+}
---
+++ Assignment 1 - CODE IT INC/Project.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1___CODE_IT_INC
+{
+   public class Project
+    {
+        //Data Members
+        private string projectName;
+        private double budget;
+        private double amountSpent;
+        private int hoursRemaining;
+        private string projectStatus;
+
+        //Constructor
+        public Project()
+        {
+            projectName = "";
+            budget = 0.00;
+            amountSpent = 0.00;
+            hoursRemaining = 0;
+            projectStatus = "";
+        }
+
+        //Getters and Setters
+        public string ProjectName
+        {
+            get { return this.projectName; }
+            set { this.projectName = value; }
+        }
+
+        public double ProjectBudget
+        {
+            get { return this.budget; }
+            set { this.budget = value; }
+        }
+
+        public double ProjectAmountSpent
+        {
+            get { return this.amountSpent; }
+            set { this.amountSpent = value; }
+        }
+
+        public int ProjectHoursRemaining
+        {
+            get { return this.hoursRemaining; }
+            set { this.hoursRemaining = value; }
+        }
+
+        public string ProjectStatus
+        {
+            get { return this.projectStatus; }
+            set { this.projectStatus = value; }
+        }
+
+    }
+}
