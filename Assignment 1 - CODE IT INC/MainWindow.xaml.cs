07de4051259c49b6436a1beb1025b9b796640a4e
Nick Sturch-Flint <nick.rulez@live.com>
2020-10-07 17:13:08 -0400

    Added a ClearText Function, and added the Observable Collection

--- Assignment 1 - CODE IT INC/MainWindow.xaml.cs
+++ Assignment 1 - CODE IT INC/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,23 +23,37 @@ namespace BillingApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Declare a public observable collection
+        public ObservableCollection<Project> projects = new ObservableCollection<Project>();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            //Add a default to the list
+            projects.Add(new Project() { ProjectName = "Durham Website", ProjectBudget = 15400.99, ProjectAmountSpent = 5023.67, ProjectHoursRemaining = 234, ProjectStatus = "Testing" });
+
+            //data bind
+            lbProjectList.ItemsSource = projects;
         }
 
         private void btnCreateProject_Click(object sender, RoutedEventArgs e)
         {
-           //Creates the project, assigns the values, and stores it in the listbox
-            Project project = new Project();
+            //Creates the project, assigns the values, and stores it in the listbox
+            projects.Add(new Project() { ProjectName = txtProjectName.Text, ProjectBudget = double.Parse(txtBudget.Text),
+            ProjectAmountSpent = double.Parse(txtSpent.Text), ProjectHoursRemaining = int.Parse(txtHoursLeft.Text), ProjectStatus = cbStatusList.Text }) ;
+
+            /* Project project = new Project();
             project.ProjectName = txtProjectName.Text;
             project.ProjectBudget = double.Parse(txtBudget.Text);
             project.ProjectAmountSpent = double.Parse(txtSpent.Text);
             project.ProjectHoursRemaining = int.Parse(txtHoursLeft.Text);
             project.ProjectStatus = cbStatusList.Text;
             lbProjectList.Items.Add(project);
+           */
 
+            //Clears the text
+            ClearText();
         }
 
 
@@ -59,6 +74,17 @@ namespace BillingApp
             newWindow.txtTimeRemainingDisplay.Text = Convert.ToString(project.ProjectHoursRemaining);
             newWindow.txtSpentDisplay.Text = Convert.ToString(project.ProjectAmountSpent);
             newWindow.cbStatusDisplay.Text = project.ProjectStatus;
+        }
+
+        //Text Clear Function
+        private void ClearText()
+        {
+            txtProjectName.Text = string.Empty;
+            txtBudget.Text = "0.00";
+            txtHoursLeft.Text = "0";
+            txtSpent.Text = "0.00";
+            cbStatusList.Text = string.Empty;
+
         }
     }
 }
